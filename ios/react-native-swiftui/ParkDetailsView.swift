fe97e9a547c35b0d57dd5c5ea4db7c13b1a1a33c
Brad Patras <7176921+BradPatras@users.noreply.github.com>
2025-02-27 15:37:50 -0600

    updated react native app

--- ios/react-native-swiftui/ParkDetailsView.swift
+++ ios/react-native-swiftui/ParkDetailsView.swift
@@ -9,32 +9,34 @@ struct ParkDetailsView: View {
 	}
 
 	var body: some View {
-		VStack(spacing: 8) {
-			AsyncImage(
-				url: URL(string: park.imageUrl),
-				content: { image in
-					image.resizable()
-						.aspectRatio(contentMode: .fill)
-						.frame(maxWidth: .infinity, maxHeight: 300)
-				},
-				placeholder: {
-					ProgressView()
-						.frame(maxWidth: .infinity, maxHeight: 300)
+		ScrollView {
+			VStack(spacing: 8) {
+				AsyncImage(
+					url: URL(string: park.imageUrl),
+					content: { image in
+						image.resizable()
+							.aspectRatio(contentMode: .fill)
+							.frame(maxWidth: .infinity, maxHeight: 300)
+					},
+					placeholder: {
+						ProgressView()
+							.frame(maxWidth: .infinity, maxHeight: 300)
+					}
+				)
+				
+				HStack {
+					Text("est. \(park.established)")
+					Spacer()
+					Text(park.location)
+					Image(systemName: "location")
 				}
-			)
-
-			HStack {
-				Text("est. \(park.established)")
-				Spacer()
-				Text(park.location)
-				Image(systemName: "location")
+				.font(.footnote)
+				
+				Text(park.description)
+					.frame(maxWidth: .infinity, alignment: .leading)
+					.multilineTextAlignment(.leading)
+					.padding(.vertical, 20)
 			}
-			.font(.footnote)
-
-			Text(park.description)
-				.frame(maxWidth: .infinity, alignment: .leading)
-				.multilineTextAlignment(.leading)
-				.padding(.vertical, 20)
 		}
 		.padding(.horizontal, 12)
 		.navigationTitle(park.name)
